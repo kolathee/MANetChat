501035ebb5b0021d2edee6cd0434da7c93548ed6
kolathee <cupthee@gmail.com>
2016-12-22 16:39:15 +0700

    Initial Commit

---
+++ MANetChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MANetChat
+//
+//  Created by Kolathee Payuhawatthana on 12/22/2559 BE.
+//  Copyright © 2559 Kolathee Payuhawattana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
