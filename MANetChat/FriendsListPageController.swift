39c95e788134533c7837b0708fe69c10ea13d438
Kolathee <cupthee@gmail.com>
2017-04-19 23:48:53 +0700

    Bug fix 	- show friends list

--- MANetChat/FriendsListPageController.swift
+++ MANetChat/FriendsListPageController.swift
@@ -6,7 +6,6 @@ import FirebaseDatabase
 class FriendsListPageController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
     let appDeledate = UIApplication.shared.delegate as? AppDelegate
-    var uidFirendsList = [String]()
     var FirstTimeViewAppear = true
     
     @IBOutlet weak var tableView: UITableView!
@@ -33,9 +32,8 @@ class FriendsListPageController: UIViewController, UITableViewDelegate,UITableVi
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "friendsCell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = appDeledate?.friends[indexPath.row].name
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendsCell", for: indexPath) as! FriendViewCell
+        cell.friendName.text = appDeledate?.friends[indexPath.row].name
         return cell
     }
     
@@ -47,35 +45,32 @@ class FriendsListPageController: UIViewController, UITableViewDelegate,UITableVi
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 51
+    }
+    
     func getFriendsListFromFirebase(){
         if let myUID = FIRAuth.auth()?.currentUser?.uid {
             //Create listener
             let ref = FIRDatabase.database().reference().child("users").child(myUID).child("friends")
             ref.observe(.value, with: { (snapshot) in
-                print("friend page : \(snapshot)")
                 //Get friends and put it into friends in AppDelegate
                 if let users = snapshot.value as? Dictionary<String,AnyObject>{
-                    print("friend page : \(users)")
                     for (key, value) in users {
                         let fName = value["name"] as! String
                         let fEmail = value["email"] as! String
                         let fUid = key
-
-                        if !self.uidFirendsList.contains(fUid){
-                            let user = User()
-                            user.uid = fUid
-                            user.email = fEmail
-                            user.name = fName
-                            self.uidFirendsList.append(fUid)
-                            //keep each user's requesting into friendsRequest in share application.
-                            self.appDeledate!.friends.append(user)
-                        }
+                        let user = User()
+                        user.uid = fUid
+                        user.email = fEmail
+                        user.name = fName
+                        //keep each user's requesting into friendsRequest in share application.
+                        self.appDeledate!.friends.append(user)
+                        print(self.appDeledate?.friends ?? "nil")
+                        self.tableView.reloadData()
                     }
                 }
-                self.tableView.reloadData()
             })
         }
     }
-    
-    
 }
