e308088f2cb3575b06d488b70c62e7db5b7f3760
kolathee <cupthee@gmail.com>
2017-05-01 02:19:28 +0700

    improve logout function. 	- show error alert when it fail to logout 	- remove all data in CoreData & AppDelegate

--- MANetChat/SettingPageController.swift
+++ MANetChat/SettingPageController.swift
@@ -13,14 +13,11 @@ import FirebaseDatabase
 class SettingPageController: UIViewController {
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var coreDataManager : CoreDataManager?
 
     override func viewDidLoad(){
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        coreDataManager = appDelegate.coreDataManager
     }
     
     func handleLogout() {
@@ -28,22 +25,34 @@ class SettingPageController: UIViewController {
         do {
         //1.Logout
             try FIRAuth.auth()?.signOut()
+            
+            //2.Remove all observers
+            let rootNodeReference = FIRDatabase.database().reference(fromURL: "https://manetchat.firebaseio.com")
+            rootNodeReference.removeAllObservers()
+            
+            //3.Remove all data
+            appDelegate.clearAllData()
+            coreDataManager?.deleteAllData(entity: "Friend")
+            coreDataManager?.deleteAllData(entity: "MyInformation")
+            
+            //4.Show Login view
+            dismiss(animated: false, completion: nil)
+            
         } catch let logoutError {
             print(logoutError)
+            showAlertMessage(title: "Fail to logout", message: "Please try again")
             return
         }
-        //2.Remove all observers
-        let rootNodeReference = FIRDatabase.database().reference(fromURL: "https://manetchat.firebaseio.com")
-        rootNodeReference.removeAllObservers()
-        
-        //3.Remove all data
-        appDelegate.clearAllData()
-        
-        //4.Show Login view
-        dismiss(animated: false, completion: nil)
     }
     
     @IBAction func logoutButtonTapped(_ sender: Any) {
         handleLogout()
+    }
+    
+    func showAlertMessage(title : String, message : String){
+        let messageWindow = UIAlertController(title: title, message:message , preferredStyle: .alert)
+        let action = UIAlertAction(title: "done", style: .cancel, handler: nil)
+        messageWindow.addAction(action)
+        self.present(messageWindow, animated: true, completion: nil)
     }
 }
